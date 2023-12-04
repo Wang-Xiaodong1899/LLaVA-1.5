6e2a37145b246a588f4b3d0f21f3f11b32251ac0
Xiaodong Wang (MAC) <1443472206@qq.com>
2023-12-04 20:27:57 +0800

    fix save lora

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -129,6 +129,30 @@ class LengthGroupedSampler(Sampler):
             indices = get_length_grouped_indices(self.lengths, self.batch_size, self.world_size, generator=self.generator)
         return iter(indices)
 
+# Borrowed from peft.utils.get_peft_model_state_dict
+def get_peft_state_maybe_zero_3(named_params, bias):
+    if bias == "none":
+        to_return = {k: t for k, t in named_params if "lora_" in k}
+    elif bias == "all":
+        to_return = {k: t for k, t in named_params if "lora_" in k or "bias" in k}
+    elif bias == "lora_only":
+        to_return = {}
+        maybe_lora_bias = {}
+        lora_bias_names = set()
+        for k, t in named_params:
+            if "lora_" in k:
+                to_return[k] = t
+                bias_name = k.split("lora_")[0] + "bias"
+                lora_bias_names.add(bias_name)
+            elif "bias" in k:
+                maybe_lora_bias[k] = t
+        for k, t in maybe_lora_bias:
+            if bias_name in lora_bias_names:
+                to_return[bias_name] = t
+    else:
+        raise NotImplementedError
+    to_return = {k: maybe_zero_3(v, ignore_status=True) for k, v in to_return.items()}
+    return to_return
 
 class LLaVATrainer(Trainer):
 
@@ -254,6 +278,13 @@ class LLaVATrainer(Trainer):
             if self.args.local_rank == 0 or self.args.local_rank == -1:
                 self.model.config.save_pretrained(output_dir)
                 torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
+        elif getattr(self.args, 'lora_enable', False):
+            state_dict = get_peft_state_maybe_zero_3(
+                model.named_parameters(), self.args.lora_bias
+            )
+            if self.args.local_rank == 0 or self.args.local_rank == -1:
+                model.config.save_pretrained(output_dir)
+                model.save_pretrained(output_dir, state_dict=state_dict)
         else:
             super(LLaVATrainer, self)._save_checkpoint(model, trial, metrics)
 
